8126a64788ae98472876f4474201a020553dd4ba
joseantoio1234 <jagonzalezm38@educarex.es>
2026-01-07 13:52:56 +0100

    desarrollo de html con distintas escenas y css solo para escena 1

---
+++ src/modules/utils.js
@@ -0,0 +1,19 @@
+export const validarNombre = (nombre) => {
+    const regla = /^[A-Z][a-zA-Z\s]{0,19}$/;
+
+    if (regla.test(nombre) && nombre.trim().lenght > 0){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+export const validarPuntos = (atq, def, vida) => {
+    const total = Number(atq) + Number(def) + Number(vida);
+
+    if (total <= 110 && atq >= 0 && def >= 0 && vida >= 100){
+        return true;
+    } else {
+        return false;
+    }
+}
